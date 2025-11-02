a02c097d456d0915baf3c967c2303b5fb1fe4606
Ashton <ashtondsouza192@gmail.com>
2025-11-02 16:00:28 +0530

    landing page work

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MoveUpRight } from "lucide-react";
 
 function landingPage() {
   return (
@@ -11,16 +12,29 @@ function landingPage() {
             </h1>
           </div>
         ))}
-        {/* <div className="masker">
-          <h1 className="uppercase text-8xl font-semibold leading-[3.5vw]  tracking-tighter font-[FoundersGrotesk]">
-            Eye-opening
-          </h1>
+      </div>
+      <div className="borderline border-t border-gray-400 mt-32  ">
+        <div className="px-5 py-3 flex justify-between items-center ">
+          {[
+            "For public and private companies",
+            "From the first pitch to IPO",
+          ].map((item, index) => (
+            <p key={index} className="font-[NeueMontreal] flex text-lg">
+              {item}
+            </p>
+          ))}
+          {/* start project div */}
+          <div className="flex gap-2">
+            <div className="start-project">
+              <div className="px-4 py-2 border-2 rounded-full  border-gray-500">
+                Start the project
+              </div>
+            </div>
+            <div>
+              <MoveUpRight className="arrow h-10  w-10 rounded-full border mt-1 border-gray-600" />
+            </div>
+          </div>
         </div>
-        <div className="masker">
-          <h1 className="uppercase text-8xl font-semibold leading-none  tracking-tighter font-[FoundersGrotesk]">
-            presentations
-          </h1>
-        </div> */}
       </div>
     </div>
   );
