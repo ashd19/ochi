c64adf316c4d25ec0a3849ff47b15f12b38cf719
Ashton <ashtondsouza192@gmail.com>
2025-11-04 14:15:06 +0530

    light border addition

--- src/components/marquee.jsx
+++ src/components/marquee.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Marquee() {
   return (
     <div className="uppercase w-full rounded-3xl  py-10 bg-[#004D43]">
-      <div className="text-white text-9xl  flex font-bold   whitespace-nowrap">
-        <h1 className="text-[20vw] leading none font-[FoundersGrotesk]">
+      <div className="text-white text-9xl  border border-white/50 flex font-bold   whitespace-nowrap">
+        <h1 className="text-[20vw] leading-none font-[FoundersGrotesk]">
           We are Ochi
         </h1>
-        <h1 className="text-[20vw] leading none font-[FoundersGrotesk] ">
+        <h1 className="text-[20vw] leading-none font-[FoundersGrotesk] ">
           We are Ochi
         </h1>
       </div>
