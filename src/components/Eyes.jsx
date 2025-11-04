e3bafee87af95905c4e16c7f1ab51ddfa7a062de
Ashton <ashtondsouza192@gmail.com>
2025-11-05 00:10:23 +0530

    latest changes wrt to the eye design

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -2,10 +2,18 @@ function Eyes() {
   return (
     <div className="w-full h-screen overflow-hidden">
       <div className="relative w-full h-full bg-cover bg-center bg-[url('src/assets/bg.jpg')]">
-        <div className="absolute w-1/2 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]   bg-red-800">
-        <div>
+        <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white">
+            <div className="w-2/3 h-2/3 flex items-center justify-center rounded-full bg-[#212121]">
+              <div className="w-1/4 h-1/4 rounded-full bg-[#ffffff]"></div>
+            </div>
+          </div>
 
-        </div>
+          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white">
+            <div className="w-2/3 h-2/3 flex items-center justify-center rounded-full bg-[#212121]">
+              <div className="w-1/4 h-1/4 rounded-full bg-[#ffffff]"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
