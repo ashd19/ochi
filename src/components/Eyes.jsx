4300844549b6ea4fd77b40c34af2aa54bf107548
Ashton <ashtondsouza192@gmail.com>
2025-11-05 12:33:04 +0530

    Added absoulte /relative instead of flex more control

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -4,14 +4,18 @@ function Eyes() {
       <div className="relative w-full h-full bg-cover bg-center bg-[url('src/assets/bg.jpg')]">
         <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white">
-            <div className="w-2/3 h-2/3 flex items-center justify-center rounded-full bg-[#212121]">
-              <div className="w-1/4 h-1/4 rounded-full bg-[#ffffff]"></div>
+            <div className="relative w-2/3 h-2/3 rounded-full bg-[#212121]">
+              <div className="line absolute  top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  w-full h-10  ">
+                <div className="w-10 h-10 rounded-full bg-[#ffffff]"></div>
+              </div>
             </div>
           </div>
 
           <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white">
-            <div className="w-2/3 h-2/3 flex items-center justify-center rounded-full bg-[#212121]">
-              <div className="w-1/4 h-1/4 rounded-full bg-[#ffffff]"></div>
+            <div className="w-2/3 h-2/3 relative rounded-full  bg-[#212121]">
+              <div className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10">
+                <div className="w-10 h-10 rounded-full bg-[#ffffff]"></div>
+              </div>
             </div>
           </div>
         </div>
