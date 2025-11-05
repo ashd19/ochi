16769ac05a6a22a0dee2694f752efd3fda4b1d09
Ashton <ashtondsouza192@gmail.com>
2025-11-05 16:13:04 +0530

    setup the animation but wrong direction currently , need to fixx  it

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,20 +1,42 @@
+import { useEffect, useState } from "react";
 function Eyes() {
+  const [rotate, setRotate] = useState(0);
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+      const deltaX = mouseX - window.innerWidth / 2;
+      const deltaY = mouseY - window.innerHeight / 2;
+      const angle = Math.atan2(deltaY, deltaX);
+      const degree = angle * (180 / Math.PI);
+      setRotate(degree - 180);
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <div className="w-full h-screen overflow-hidden">
       <div className="relative w-full h-full bg-cover bg-center bg-[url('src/assets/bg.jpg')]">
         <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white">
             <div className="relative w-2/3 h-2/3 rounded-full bg-[#212121]">
-              <div className="line absolute  top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  w-full h-10  ">
-                <div className="w-10 h-10 rounded-full bg-[#ffffff]"></div>
+              <div
+                style={{ transform: `rotate(${rotate}deg)` }}
+                className="line w-full  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              >
+                <div className="w-10 h-10 rounded-full bg-white"></div>
               </div>
             </div>
           </div>
 
           <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white">
             <div className="w-2/3 h-2/3 relative rounded-full  bg-[#212121]">
-              <div className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10">
-                <div className="w-10 h-10 rounded-full bg-[#ffffff]"></div>
+              <div
+                style={{ transform: `rotate(${rotate}deg)` }}
+                className="line w-full  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              >
+                <div className="w-10 h-10 rounded-full bg-white"></div>
               </div>
             </div>
           </div>
